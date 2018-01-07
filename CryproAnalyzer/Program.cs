2c4ba5d6884902d1db8c76f8c1a8073d07381eeb
Dmitry <rdima.96@yandex.ru>
2018-01-08 02:57:44 +0400

    Инициализация

--- CryproAnalyzer/Program.cs
+++ CryproAnalyzer/Program.cs
@@ -1,15 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Bittrex.Api.Client;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace CryproAnalyzer
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        private const string BittrexKey = "bdccc7011d0149abba1b7259f9fab600";
+        private const string BittrexSecret = "bba32eed2e2341d69ff344f9640f21c4";
+
+        private const string TelegramToken = "517696499:AAFd655vCUB51agOd9_jrepHQXoHdbDvvCQ";
+
+
+        public static void Main(string[] args)
         {
+            var botClient = new TelegramBotClient(TelegramToken);
+            var bittrexClient = new BittrexClient(BittrexKey, BittrexSecret);
+
+
+            var markets = bittrexClient.GetMarkets();
+
+            var offset = 0;
+
+            while (true)
+            {
+                var updates = botClient.GetUpdatesAsync(offset: offset, timeout: 100);
+
+                foreach (var update in updates.Result)
+                {
+                    offset = update.Id + 1;
+
+                    if (update.Message.Type == MessageType.TextMessage)
+                    {
+                        var chatId = update.Message.Chat.Id;
+                        var messageText = update.Message.Text;
+
+
+                        var responseText = "";
+                        IReplyMarkup replyMarkup = null;
+
+                        switch (messageText)
+                        {
+                            case "/start":
+                                responseText = "Добро пожаловать в чат-бот. Подпишься на сигналы.";
+                                replyMarkup = new ReplyKeyboardMarkup(new[]
+                                        {new KeyboardButton("Подписаться")},
+                                    resizeKeyboard: true);
+                                break;
+                            case "Подписаться":
+                                responseText = "Спасибо за подписку!";
+                                replyMarkup = new ReplyKeyboardMarkup(new[]
+                                        {new KeyboardButton("Отписаться")},
+                                    resizeKeyboard: true);
+                                break;
+                            case "Отписаться":
+                                responseText = "Ты всегда можешь подписаться снова!";
+                                replyMarkup = new ReplyKeyboardMarkup(new[]
+                                        {new KeyboardButton("Подписаться")},
+                                    resizeKeyboard: true);
+                                break;
+                        }
+
+                        botClient.SendTextMessageAsync(chatId, responseText,
+                            replyMarkup: replyMarkup);
+                    }
+                }
+            }
         }
     }
 }
