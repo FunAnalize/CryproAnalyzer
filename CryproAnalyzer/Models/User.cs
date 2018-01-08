efe677cd71ff5191a1d44f5c923a6dbaccc1ed8a
Виктор Грэм <orlenokal@rambler.ru>
2018-01-08 05:07:59 +0400

    Добавление сущностей

---
+++ CryproAnalyzer/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryproAnalyzer.Models
+{
+    class User
+    {
+        [Key]
+        public string ChatID { get; set; }
+        public bool IsSubscriber { get; set; }
+    }
+}
