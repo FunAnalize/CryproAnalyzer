7ed32f482a027175c85ab027315b661248838970
Виктор Грэм <orlenokal@rambler.ru>
2018-01-08 04:33:21 +0400

    Добавлено вычисление средней

--- CryproAnalyzer/Analyzers/LowerAvergeAnalyzer.cs
+++ CryproAnalyzer/Analyzers/LowerAvergeAnalyzer.cs
@@ -1,12 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Bittrex.Net;
+using Bittrex.Net.Objects;
 
 namespace CryproAnalyzer.Analyzers
 {
     class LowerAvergeAnalyzer
     {
+        private readonly BittrexClient _client;
+
+        public LowerAvergeAnalyzer(BittrexClient client)
+        {
+            _client = client;
+        }
+
+        public LowerAvergeAnalyzerResult Analyze(string marketName,int dayInterval)
+        {
+            try
+            {
+                decimal result=0m;
+                int index = 0;
+                var candels = _client.GetCandles(marketName, TickInterval.HalfHour).Result;
+                foreach (var candle in candels.Where(p=>p.Timestamp>DateTime.Now.AddDays(-dayInterval)))
+                {
+                    result+=(candle.Open + candle.Close) / 2;
+                    index++;
+                }
+
+                var currentPrice = _client.GetTicker(marketName).Result.Bid;
+                var average = result / index;
+                if (currentPrice < average)
+                {
+                    return new LowerAvergeAnalyzerResult
+                        { MarketName = marketName,Average = average,Current = currentPrice,GoodBuy = true,Percent = average/currentPrice*100};
+
+                }
+                return new LowerAvergeAnalyzerResult
+                    { MarketName = marketName, Average = average, Current = currentPrice,GoodBuy = false, Percent = average / currentPrice*100 };
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+                return new LowerAvergeAnalyzerResult();
+            }
+        }
+    }
+
+    class LowerAvergeAnalyzerResult
+    {
+        public decimal Average { get; set; }
+        public decimal Current { get; set; }
+        public string MarketName { get; set; }
+        public bool GoodBuy { get; set; }
+        public decimal Percent { get; set; }
     }
 }
