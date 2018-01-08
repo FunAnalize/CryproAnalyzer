bf2bcd2a1360aaa3cc868148c60874afa3a93098
Виктор Грэм <orlenokal@rambler.ru>
2018-01-08 06:21:00 +0400

    Добавлена запись пользователей в базу данных

--- CryproAnalyzer/Program.cs
+++ CryproAnalyzer/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Bittrex.Net;
+using CryproAnalyzer.Models;
+using CryproAnalyzer.Telegram;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
@@ -16,57 +18,11 @@ namespace CryproAnalyzer
 
         public static void Main(string[] args)
         {
-            var botClient = new TelegramBotClient(TelegramToken);
-            var bittrexClient = new BittrexClient(BittrexKey, BittrexSecret);
-
-
-            var markets = bittrexClient.GetMarkets();
-
-            var offset = 0;
-
+            BotClient bc = new BotClient(TelegramToken);
+            bc.Start();
             while (true)
             {
-                var updates = botClient.GetUpdatesAsync(offset: offset, timeout: 100);
-
-                foreach (var update in updates.Result)
-                {
-                    offset = update.Id + 1;
-
-                    if (update.Message.Type == MessageType.TextMessage)
-                    {
-                        var chatId = update.Message.Chat.Id;
-                        var messageText = update.Message.Text;
-
-
-                        var responseText = "";
-                        IReplyMarkup replyMarkup = null;
-
-                        switch (messageText)
-                        {
-                            case "/start":
-                                responseText = "Добро пожаловать в чат-бот. Подпишься на сигналы.";
-                                replyMarkup = new ReplyKeyboardMarkup(new[]
-                                        {new KeyboardButton("Подписаться")},
-                                    resizeKeyboard: true);
-                                break;
-                            case "Подписаться":
-                                responseText = "Спасибо за подписку!";
-                                replyMarkup = new ReplyKeyboardMarkup(new[]
-                                        {new KeyboardButton("Отписаться")},
-                                    resizeKeyboard: true);
-                                break;
-                            case "Отписаться":
-                                responseText = "Ты всегда можешь подписаться снова!";
-                                replyMarkup = new ReplyKeyboardMarkup(new[]
-                                        {new KeyboardButton("Подписаться")},
-                                    resizeKeyboard: true);
-                                break;
-                        }
-
-                        botClient.SendTextMessageAsync(chatId, responseText,
-                            replyMarkup: replyMarkup);
-                    }
-                }
+                
             }
         }
     }
---
+++ CryproAnalyzer/Telegram/BotClient.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading;
+using CryproAnalyzer.Models;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace CryproAnalyzer.Telegram
+{
+    class BotClient
+    {
+        readonly TelegramBotClient _botClient;
+        private readonly Thread _thread;
+        private int _offset;
+
+        public BotClient(string telegramToken)
+        {
+            _thread=new Thread(Act);
+            _botClient = new TelegramBotClient(telegramToken);
+        }
+
+        public void Start()
+        {
+            _thread.Start();
+        }
+
+        public void Stop()
+        {
+            _thread.Interrupt();
+        }
+        private void Act()
+        {
+            while (true)
+            {
+                var updates = _botClient.GetUpdatesAsync(offset: _offset, timeout: 100);
+
+                foreach (var update in updates.Result)
+                {
+                    _offset = update.Id + 1;
+
+                    if (update.Message.Type == MessageType.TextMessage)
+                    {
+                        var chatId = update.Message.Chat.Id;
+                        var messageText = update.Message.Text;
+
+                        bool? isSubscribe=null;
+                        var responseText = "";
+                        IReplyMarkup replyMarkup = null;
+
+                        switch (messageText)
+                        {
+                            case "/start":
+                                responseText = "Добро пожаловать в чат-бот. Подпишься на сигналы.";
+                                replyMarkup = new ReplyKeyboardMarkup(new[]
+                                        {new KeyboardButton("Подписаться")},
+                                    resizeKeyboard: true);
+                                isSubscribe = false;
+                                break;
+                            case "Подписаться":
+                                responseText = "Спасибо за подписку!";
+                                replyMarkup = new ReplyKeyboardMarkup(new[]
+                                        {new KeyboardButton("Отписаться")},
+                                    resizeKeyboard: true);
+                                isSubscribe = true;
+                                break;
+                            case "Отписаться":
+                                responseText = "Ты всегда можешь подписаться снова!";
+                                replyMarkup = new ReplyKeyboardMarkup(new[]
+                                        {new KeyboardButton("Подписаться")},
+                                    resizeKeyboard: true);
+                                isSubscribe = false;
+                                break;
+                        }
+
+                        _botClient.SendTextMessageAsync(chatId, responseText,
+                            replyMarkup: replyMarkup);
+
+                        if (!(isSubscribe is null))
+                        {
+                            DbUpdate(chatId, isSubscribe.Value);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void DbUpdate(long chatId, bool isSubscribe)
+        {
+            using (AnalyzerContext db = new AnalyzerContext())
+            {
+                var user = db.Users.FirstOrDefault(p=>p.ChatId==chatId);
+                if (user is null)
+                {
+                    db.Users.Add(new Models.User {ChatId = chatId, IsSubscribed = isSubscribe});
+                }
+                else
+                {
+                    user.IsSubscribed = isSubscribe;
+                    db.Entry(user).State = EntityState.Modified;
+                }
+                db.SaveChanges();
+            }
+        }
+    }
+}
