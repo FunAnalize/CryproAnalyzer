455a8bc3d4efaf9848fe44a877f2b3b3cb5dfee1
Dmitry <rdima.96@yandex.ru>
2018-02-01 13:43:28 +0400

    Добавлена миграция для модели сигналов.

--- Models/Migrations/20180201094221_Signals.Designer.cs
+++ Models/Migrations/20180201094221_Signals.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Models.Migrations
 {
     [DbContext(typeof(AnalyzerContext))]
-    partial class AnalyzerContextModelSnapshot : ModelSnapshot
+    [Migration("20180201094221_Signals")]
+    partial class Signals
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Models/Migrations/20180201094221_Signals.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Models.Migrations
+{
+    public partial class Signals : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Signals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Average = table.Column<decimal>(type: "numeric(30,28)", nullable: false),
+                    Interval = table.Column<int>(nullable: false),
+                    MarketName = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(type: "numeric(30,28)", nullable: false),
+                    Ratio = table.Column<decimal>(type: "numeric(30,28)", nullable: true),
+                    Timestamp = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Signals", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SignalResults",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Price = table.Column<decimal>(type: "numeric(30,28)", nullable: false),
+                    SignalId = table.Column<int>(nullable: false),
+                    Timestamp = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SignalResults", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SignalResults_Signals_SignalId",
+                        column: x => x.SignalId,
+                        principalTable: "Signals",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SignalResults_SignalId",
+                table: "SignalResults",
+                column: "SignalId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SignalResults");
+
+            migrationBuilder.DropTable(
+                name: "Signals");
+        }
+    }
+}
